6f9a592765f92ee4fbee2a1c9023e20332c9eaa7
high king <116436670+sohasanat@users.noreply.github.com>
2024-02-14 14:18:27 +0330

    password

---
+++ app/Mail/VerifyCodeEmail.php
@@ -0,0 +1,59 @@
+<?php
+
+// File: app/Mail/VerifyCodeEmail.php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class VerifyCodeEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $verificationCode;
+
+    public function __construct($verificationCode)
+    {
+        $this->verificationCode = $verificationCode;
+    }
+
+    public function build()
+    {
+        return $this->view('emails.verify_code_email');
+    }
+}
+
+    // /**
+    //  * Get the message envelope.
+    //  */
+    // public function envelope(): Envelope
+    // {
+    //     return new Envelope(
+    //         subject: 'Verify Code Email',
+    //     );
+    // }
+
+    // /**
+    //  * Get the message content definition.
+    //  */
+    // public function content(): Content
+    // {
+    //     return new Content(
+    //         view: 'view.name',
+    //     );
+    // }
+
+    // /**
+    //  * Get the attachments for the message.
+    //  *
+    //  * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+    //  */
+    // public function attachments(): array
+    // {
+    //     return [];
+    // }
+// }
--- resources/views/lost-password.blade.php
+++ resources/views/lost-password.blade.php
@@ -1,42 +1,50 @@
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../node_modules/swiper/swiper-bundle.min.css">
-    <link rel="stylesheet" href="build/style.css">
-    <title>فراموشی رمز عبور</title><link rel="icon" type="image/x-icon" href="../assets/images/favicon.ico">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../node_modules/swiper/swiper-bundle.min.css">
+  <link rel="stylesheet" href="build/style.css">
+  <title>فراموشی رمز عبور</title>
+  <link rel="icon" type="image/x-icon" href="../assets/images/favicon.ico">
 </head>
+
 <body class="font-YekanBakh-Regular text-sm bg-[#f5f1e4]">
 
-    <div class="min-h-screen py-6 flex flex-col justify-center sm:py-12">
-        <div class="relative py-3 sm:max-w-sm sm:mx-auto">
-          <div
-            class="absolute inset-0 bg-stone-900 transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
-          </div>
-          <div class="relative px-4 py-10 bg-gradient-to-t from-orange-100 to-[#f5f1e4] sm:rounded-3xl sm:p-14">
-      
-            <div class="max-w-md mx-auto">
-              <div>
-                <h1 class="text-xl text-center font-YekanBakh-Bold">فراموشی رمز عبور</h1>
-              </div>
-              <div class="divide-y divide-gray-200">
-                <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                  <p class="text-xs text-gray-600 text-center leading-8">
-                   ایمیل خود را وارد کنید. یک لینک برای ساختن گذرواژه جدید در ایمیل خود دریافت خواهید کرد.
-                  </p>
-                    <input type="email" placeholder="پست الکترونیکی:" class="input w-full rounded-full focus:outline-none placeholder:text-sm" />
-                    <button class="btn bg-stone-800 w-full hover:bg-stone-900 text-white rounded-full text-sm">بازیابی گذرواژه</button>
-                    
-                </div>
+  <div class="min-h-screen py-6 flex flex-col justify-center sm:py-12">
+    <div class="relative py-3 sm:max-w-sm sm:mx-auto">
+      <div class="absolute inset-0 bg-stone-900 transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+      </div>
+      <div class="relative px-4 py-10 bg-gradient-to-t from-orange-100 to-[#f5f1e4] sm:rounded-3xl sm:p-14">
+
+        <form action="{{ route('user.losstpasvrify') }}" method="POST" enctype="multipart/form-data">
+          @csrf
+          <div class="max-w-md mx-auto">
+            <div>
+              <h1 class="text-xl text-center font-YekanBakh-Bold">فراموشی رمز عبور</h1>
+            </div>
+            <div class="divide-y divide-gray-200">
+              <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <p class="text-xs text-gray-600 text-center leading-8">
+                  ایمیل خود را وارد کنید. یک لینک برای ساختن گذرواژه جدید در ایمیل خود دریافت خواهید کرد.
+                </p>
+                <input type="email" placeholder="پست الکترونیکی:" class="input w-full rounded-full focus:outline-none placeholder:text-sm" />
+                <button class="btn bg-stone-800 w-full hover:bg-stone-900 text-white rounded-full text-sm" type="submit">
+                  <a href="/lost-pass-code">بازیابی گذرواژه</a>
+
+                </button>
               </div>
             </div>
-      
           </div>
-        </div>
-      </div>
+        </form>
 
-    <script src="../node_modules/swiper/swiper-bundle.min.js"></script>
-    <script src="../src/js/main.js"></script>
+      </div>
+    </div>
+  </div>
+
+  <script src="../node_modules/swiper/swiper-bundle.min.js"></script>
+  <script src="../src/js/main.js"></script>
 </body>
+
 </html>
